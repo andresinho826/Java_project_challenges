dec18771883ad28f0f1e9e89958826a729adcf3d
Andres Fernando Castillo Moreno <af.castillo@globant.com>
2024-06-26 13:22:44 -0500

    exercise fizz buzz

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ examples to practice java
 
 Website:   https://retosdeprogramacion.com/ejercicios
 
+1ro: el primer ejercicio es el famoso fizz buzz
+
+
+
 
--- src/main/java/codigo_practice/FizzBuzz.java
+++ src/main/java/codigo_practice/FizzBuzz.java
@@ -1,4 +1,39 @@
 package codigo_practice;
 
 public class FizzBuzz {
+
+    /*
+     * Escribe un programa que muestre por consola (con un print) los
+     * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+     * cada impresión), sustituyendo los siguientes:
+     * - Múltiplos de 3 por la palabra "fizz".
+     * - Múltiplos de 5 por la palabra "buzz".
+     * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+     */
+
+    public void evaluate(int n){
+
+
+        for (int i = 1; i <= n; i++){
+            Boolean fizThre = (i % 3) == 0;
+            Boolean buzFive = (i % 5) == 0;
+
+            if (fizThre && buzFive){
+                System.out.println("fizzbuzz");
+            } else if (fizThre) {
+                System.out.println("fizz");
+
+            } else if (buzFive) {
+                System.out.println("buzz");
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        fb.evaluate(15);
+
+    }
 }
